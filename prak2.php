feafbffa1b231f84acd9a2fcb5aae32547284b40
MUHAMMAD KUS PRASETYO <101635875+2100018090@users.noreply.github.com>
2022-07-16 20:14:35 +0700

    Add files via upload

---
+++ prak2.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tugas Proyek PWEB </title>
+	<link rel="stylesheet" type="text/css" href="pweb3.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<!-- loader -->
+	<div class="bg-loader">
+		<div class="loader"></div>
+	</div>
+
+	<!--header-->
+	<div class="medsos">
+		<div class="container">
+			<ul>
+				<li><a href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a></li>
+				<li><a href="https://www.youtube.com/?gl=DE"><i class="fa-brands fa-youtube"></i></i></a></li>
+				<li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></i></a></li>
+				<li><a href="https://twitter.com/?lang=en-id"><i class="fa-brands fa-twitter"></i></i></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<header>
+		<div class="container">
+		<h1><a href="prak1.php">MY WORLD </a></h1>
+		<ul>
+			<li><a href="prak1.php">HOME</a></li>
+			<li class="active"><a href="prak2.php">CERITA</a></li>
+			<li><a href="prak3.php">SERVICE</a></li>
+			<li><a href="prak4.php">CONTACT</a></li>
+		</ul>
+		</div>
+	</header>
+
+	<!-- label -->
+	<section class="label">
+		<div class="container">
+			<p>Home/About</p>
+		</div>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<img src="EWRxarmUYAEIBG-.jpg " width="100%">
+			<h3>CERITA NABI MUHAMMAD SAW</h3>
+			<p>1. Kelahiran Nabi Muhammad Nabi SAW</p>
+<p>Nabi Muhammad lahir di Makkah hari Senin, 12 Rabi’ul Awal pada tahun 571 kalender Romawi (1450 tahun yang lalu). Rasul lahir dari ibu bernama Aminah dan ayahnya bernama Abdullah. Tahun tersebut juga disebut sebagai Tahun Gajah yakni tahun ketika pasukan gajah di bawah pimpinan Abrahah Habasyah tengah menyerang Ka’bah. Allah SWT pun menghentikan aksi mereka dengan segala kebesaranNya. Burung ababil pun datang menjatuh batu-batu untuk mendatangkan wabah penyakit.
+Kisah kelahiran Nabi Muhammad ini ada di dalam Surah Al Fil yang memiliki arti Tahun Gajah. Rasulullah lahir di masa ini dan dibesarkan sebagai anak yatim karena ayahnya, Abdullah telah meninggal dunia sebelum usianya genap 3 Tahun. Semasa kecilnya, akhirnya dibesarkan oleh kakeknya, Abdul Muthalib.
+Pada saat Nabi lahir, seorang ibu bernama Halimah Sa’diyah dengan ikhlas mau menyusui Muhammad meski ASI-nya sulit keluar. Namun karena Keikhlasan Halimah pun diberi balasan oleh Allah SWT, karena setelah itu air ASI-nya keluar dengan deras.</p>
+<p>2. Masa Kecil Nabi Muhammad</p>
+<p>Nabi Muhammad dilahirkan dalam keadaan yatim di rumah Abu Talib. Semasa kecilnya, Rasulullah tumbuh dan menjalani kehidupannya seperti pada anak pada umumnya. Adanya tradisi Quraisy pada zaman dahulu, pada hari kedelapan belas membuat ibunya harus menyembunyikannya di pedalaman.
+Tradisi Quraisy tersebut membuat Nabi Muhammad tidak bisa merasakan kasih sayang ibunya sampai berumur 8 sampai 10 tahun. Hal ini justru membuatnya berada di bawah asuhan Halimah binti Sa’diyah selama tiga tahun. Rasulullah menjadi anak yang tanggap, bersikap baik dan cerdas pada masanya.</p>
+<p>3. Masa Remaja Nabi Muhammad</p>
+<p>Pada masa Remasa, Nabi Muhammad terjaga dari perbuatan merugikan kawan sekitarnya. Sampai suatu ketika, Nabi pun bercerita ketika dua kali duduk saat mendengarkan pesta perkawinan di zaman Jahiliyah.</p>
+<p>Allah justru menutup telinganya sampai tertidur dan terbangun esoknya. “Setelah itu, aku tidak pernah lagi berniat mengikuti perbuatan buruk.” (HR Thabrani).</p>
+<p>Muhammad yang menginjak usia 20 tahun di Mekah yang bertepatan peristiwa Harbul Fijar antara Kabilah Quraisy melawan Qais dan Aylan.</p>
+<p>4. Nabi Muhammad Menjelang Dewasa</p>
+<p>Menjelang usia Nabi Muhammad yang dewasa, membuatnya semakin menekuni dunia bisnis. Nabi pun berdagang dengan kawan terbaiknya yakni Saib bin Abi Saib. Barulah pada saat berusia 25 tahun, Rasulullah menjalin kerja sama bisnis bersama wanita kaya raya yakni Siti Khadijah.</p>
+<p>Perkenalan Muhammad dengan Khadijah memang berawal dari dunia perniagaan. Perempuan ini biasa membiayai kafilah perdagangan Mekkah ke Suriah untuk nanti membagi keuntungan bersama mitranya. Hal ini menjadi alasan bagi mereka berdua dalam melakukan perjalanan dagang tersebut.</p>
+<p>5. Pernikahan Nabi Muhammad dan Khadijah</p>
+<p>Banyaknya kegiatan perdagangan yang melibatkan mereka berdua, membuat Khadijah merasa kian tertarik. Perempuan ini akhirnya mengutus seorang sahabatnya, Nafisah binti Umayyah untuk menyampaikan keinginannya yakni melamar Muhammad.</p>
+<p>Muhammad SAW pun menyampaikan kabar gembira ini kepada paman-pamannya. Salah satunya yakni, Hamzah bin Abdul Muthalib lantas mendatangi rumah Khuwailid bin Asad dengan Muhammad untuk melamar Khadijah. Maka menikahlah mereka berdua ketika Nabi berusia 28 tahun.</p>
+<p>6. Nabi Muhammad Mendapatkan Wahyu Pertama</p>
+<p>Sebelum menjadi Rasul, Nabi Muhammad sudah mendapatkan beberapa karunia istimewa dari Allah seperti wajahnya terlihat bersinar dan bersih. Hal ini nyatanya menjadi pertanda kebesaran Allah yang menandakan akan datangnya nabi terakhir dengan kedudukan tertinggi sampai akhir zaman.</p>
+<p>Nabi Muhammad mendapatkan sebuah mimpi ketika Malaikat Jibril menghampirinya. Rasul pun sedang menyendiri di dalam Gua Hira tepatnya di samping Jabal Nur. Turunlah wahyu pertama yang ia bawakan dari Allah yakni Surah Al – “Alaq 1 – 4.</p>
+<p>7. Dakwah Pertama Nabi Muhammad</p>
+<p>Nabi Muhammad akhirnya memulai dakwahnya secara terang-terangan pada keluarga paling dekat yaitu kalangan Bani Hasyim. Hanya Ali bin Abu Thalib yang mau menerima dan memutuskan untuk beriman kepada Allah. Sementara Abu Thalib ikut melindungi Rasul saat berdakwah.</p>
+<p>Dakwah secara terang-terangan ini selalu mendapatkan pertentangan oleh kaum Quraisy. Bahkan beberapa orang menuduh Nabi Muhammad gila dan melemparkan kotoran ke tubuh Nabi. Abu Jahal dan Abu Lahab sebagai pamannya bahkan juga ikut menentang Rasul selama berdakwah.</p>
+
+
+			</p>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>Copyright &vopy; 2022 - Prasetyo. All Rights Reserved.</small>
+		</div>
+	</footer>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$(".bg-loader").hide();
+		})
+	</script>
+
+</body>
+</html>
